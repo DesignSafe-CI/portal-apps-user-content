fc0ecc7ce113641d4ec999819cf781284bb303a8
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-06-20 14:00:28 -0500

    docs(opensees): add to comments in javascript

--- opensees/formatting.js
+++ opensees/formatting.js
@@ -5,13 +5,15 @@ const interactiveVariant = document.getElementById('app-var-6-v');
 
 const desc = interactiveVariant.querySelector('p');
 desc.innerHTML = desc.innerHTML
-    /* to soften voice */
+    // to soften voice
     .replace(
         'If your connection fails, please use JupyterHub instead.',
         '<em>If your connection fails, please use JupyterHub instead.</em>'
     )
-    /* to link to JupyterHub */
+    // to link to JupyterHub
+    // FAQ: extra `<span>` prevents style bug from HTML assumption in CSS
+    // SEE: https://github.com/DesignSafe-CI/portal/blob/v7.4.1/designsafe/static/styles/app-version-list.css#L54-L64
     .replace(
         'JupyterHub',
         '<a href="/use-designsafe/tools-applications/analysis/jupyter/">JupyterHub</a><span></span>'
-    ); // FAQ: Extra `<span>` prevents style bug from HTML assumption in CSS
+    );
