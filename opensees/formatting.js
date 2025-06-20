e0fc317b3a07499eb72777b38e2dd223019564d2
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-06-20 14:03:53 -0500

    fix(opensees): prevent error if element is missing

--- opensees/formatting.js
+++ opensees/formatting.js
@@ -1,19 +1,25 @@
 // SEE: /admin/.../snippet/26
 // SEE: /use-designsafe/tools-applications/simulation/opensees/
 
-const interactiveVariant = document.getElementById('app-var-6-v');
+const interactiveVMapp = document.getElementById('app-var-6-v');
 
-const desc = interactiveVariant.querySelector('p');
-desc.innerHTML = desc.innerHTML
-    // to soften voice
-    .replace(
-        'If your connection fails, please use JupyterHub instead.',
-        '<em>If your connection fails, please use JupyterHub instead.</em>'
-    )
-    // to link to JupyterHub
-    // FAQ: extra `<span>` prevents style bug from HTML assumption in CSS
-    // SEE: https://github.com/DesignSafe-CI/portal/blob/v7.4.1/designsafe/static/styles/app-version-list.css#L54-L64
-    .replace(
-        'JupyterHub',
-        '<a href="/use-designsafe/tools-applications/analysis/jupyter/">JupyterHub</a><span></span>'
+if (interactiveVMapp) {
+    const desc = interactiveVMapp.querySelector('p');
+    desc.innerHTML = desc.innerHTML
+        // to soften voice
+        .replace(
+            'If your connection fails, please use JupyterHub instead.',
+            '<em>If your connection fails, please use JupyterHub instead.</em>'
+        )
+        // to link to JupyterHub
+        // FAQ: extra `<span>` prevents style bug from HTML assumption in CSS
+        // SEE: https://github.com/DesignSafe-CI/portal/blob/v7.4.1/designsafe/static/styles/app-version-list.css#L54-L64
+        .replace(
+            'JupyterHub',
+            '<a href="/use-designsafe/tools-applications/analysis/jupyter/">JupyterHub</a><span></span>'
+        );
+} else {
+    console.warn(
+        'Interactive VM App element not found. Please check the HTML structure.'
     );
+}
